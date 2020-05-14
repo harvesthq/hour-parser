edb927b2230d2b4509763288dc5ece417dad5da7
Evan Walsh <evan@getharvest.com>
2020-05-14 13:52:15 -0400

    Build as ES2020

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
-declare var _default: {
+declare const _default: {
+    /**
+     * Convert timestamp to decimal format
+     * @param {number|string} input? Timestamp to convert
+     * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
+     */
     toDecimal(input?: string | number | undefined): string | number | undefined;
+    /** Convert timestamp to hh:mm format
+     * @param {number|string} input? Timestamp to convert
+     * @returns {string} A timestamp in hh:mm format
+     */
     toHHMM(input?: string | number | undefined): string | number | undefined;
 };
 export default _default;
--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var parseNumber = function (input) {
-    var _a = input.toString().replace(/,/, '.').split(':'), hours = _a[0], minutes = _a[1];
-    var sign = /^\s*-/.test(hours) ? '-' : '';
+const parseNumber = (input) => {
+    const [hours, minutes] = input.toString()
+        .replace(/,/g, '.')
+        .replace(/\s/g, '')
+        .split(':');
+    const sign = /^\s*-/.test(hours) ? '-' : '';
     return 60 * parseFloat(hours || '0') + parseFloat(sign + (minutes || '0'));
 };
-var evalInput = function (input) {
-    var adder = function (sum, match) { return sum + parseNumber(match); };
+const evalInput = (input) => {
+    const adder = (sum, match) => sum + parseNumber(match);
     return input.toString().match(/\s*[+-]?[^+-]+/g).reduce(adder, 0);
 };
 exports.default = {
     /**
-     * Convert timestamp to decimal format (rounded to 2 decimal places)
-     * @param  {number|string} input? Timestamp to convert
+     * Convert timestamp to decimal format
+     * @param {number|string} input? Timestamp to convert
+     * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
      */
-    toDecimal: function (input) {
+    toDecimal(input) {
         if (!input && input !== 0) {
             return input;
         }
         if (typeof (input) === 'number') {
             return input.toFixed(2);
         }
-        var hours = evalInput(input) / 60;
+        const hours = evalInput(input) / 60;
         return (isNaN(hours) ? '0.00' : hours.toFixed(2).toString());
     },
     /** Convert timestamp to hh:mm format
-     * @param  {number|string} input? Timestamp to convert
+     * @param {number|string} input? Timestamp to convert
+     * @returns {string} A timestamp in hh:mm format
      */
-    toHHMM: function (input) {
+    toHHMM(input) {
         if (!input && input !== 0) {
             return input;
         }
-        var total = evalInput(input);
+        let total = evalInput(input);
         if (isNaN(total)) {
             return '';
         }
-        var sign = total < 0 ? '-' : '';
+        const sign = total < 0 ? '-' : '';
         total = Math.abs(total);
-        var hours = Math.floor(total / 60);
-        var minutes = Math.round(total) % 60;
-        return "" + sign + hours + ":" + (minutes < 10 ? "0" + minutes : minutes);
+        const hours = Math.floor(total / 60);
+        const minutes = Math.round(total) % 60;
+        const paddedMinutes = minutes.toString().padStart(2, '0');
+        return `${sign}${hours}:${paddedMinutes}`;
     }
 };
