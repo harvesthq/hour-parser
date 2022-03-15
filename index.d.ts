5dc193175029e16e00668bbaba7453e81826c3a2
Evan Walsh <evan@hellowelcome.org>
2022-03-15 12:39:24 -0400

    Build v2.0

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,11 @@
-declare const _default: {
-    /**
-     * Convert timestamp to decimal format
-     * @param {number|string} input? Timestamp to convert
-     * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
-     */
-    toDecimal(input?: string | number | undefined): string;
-    /** Convert timestamp to hh:mm format
-     * @param {number|string} input? Timestamp to convert
-     * @returns {string} A timestamp in hh:mm format
-     */
-    toHHMM(input?: string | number | undefined): string;
-};
-export default _default;
+/**
+ * Convert timestamp to decimal format
+ * @param {number|string} input? Timestamp to convert
+ * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
+ */
+export declare const toDecimal: (input?: string | number | undefined) => string;
+/** Convert timestamp to hh:mm format
+ * @param {number|string} input? Timestamp to convert
+ * @returns {string} A timestamp in hh:mm format
+ */
+export declare const toHHMM: (input?: string | number | undefined) => string;
--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.toHHMM = exports.toDecimal = void 0;
+/**
+ * Convert timestamp to decimal format
+ * @param {number|string} input? Timestamp to convert
+ * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
+ */
+const toDecimal = (input) => {
+    if (!input && input !== 0) {
+        return '';
+    }
+    if (typeof input === 'number') {
+        return input.toFixed(2);
+    }
+    const hours = evalInput(input) / 60;
+    return isNaN(hours) ? '' : hours.toFixed(2).toString();
+};
+exports.toDecimal = toDecimal;
+/** Convert timestamp to hh:mm format
+ * @param {number|string} input? Timestamp to convert
+ * @returns {string} A timestamp in hh:mm format
+ */
+const toHHMM = (input) => {
+    if (!input && input !== 0) {
+        return '';
+    }
+    let total = evalInput(input);
+    if (isNaN(total)) {
+        return '';
+    }
+    const sign = total < 0 ? '-' : '';
+    total = Math.abs(total);
+    const hours = Math.floor(total / 60);
+    const minutes = Math.round(total) % 60;
+    const paddedMinutes = minutes.toString().padStart(2, '0');
+    return `${sign}${hours}:${paddedMinutes}`;
+};
+exports.toHHMM = toHHMM;
+// Helpers
 const parseNumber = (input) => {
     const [hours, minutes] = input
         .toString()
@@ -15,40 +53,4 @@ const evalInput = (input) => {
         .toString()
         .match(/\s*[+-]?[^+-]+/g)
         .reduce(adder, 0);
-};
-exports.default = {
-    /**
-     * Convert timestamp to decimal format
-     * @param {number|string} input? Timestamp to convert
-     * @returns {string} A timestamp in decimal format (rounded/padded to 2 decimals places)
-     */
-    toDecimal(input) {
-        if (!input && input !== 0) {
-            return '';
-        }
-        if (typeof input === 'number') {
-            return input.toFixed(2);
-        }
-        const hours = evalInput(input) / 60;
-        return isNaN(hours) ? '' : hours.toFixed(2).toString();
-    },
-    /** Convert timestamp to hh:mm format
-     * @param {number|string} input? Timestamp to convert
-     * @returns {string} A timestamp in hh:mm format
-     */
-    toHHMM(input) {
-        if (!input && input !== 0) {
-            return '';
-        }
-        let total = evalInput(input);
-        if (isNaN(total)) {
-            return '';
-        }
-        const sign = total < 0 ? '-' : '';
-        total = Math.abs(total);
-        const hours = Math.floor(total / 60);
-        const minutes = Math.round(total) % 60;
-        const paddedMinutes = minutes.toString().padStart(2, '0');
-        return `${sign}${hours}:${paddedMinutes}`;
-    },
 };
